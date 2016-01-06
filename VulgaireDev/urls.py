8a1492b6fab25a1f89afdd5a2ccd13d3cc87c063
Winternis <nicolas_nativel_974@hotmail.fr>
2016-01-06 21:06:54 +0100

    test branche

--- VulgaireDev/urls.py
+++ VulgaireDev/urls.py
@@ -1,4 +1,4 @@
-"""VulgaireDev URL Configurations test
+"""VulgaireDev URL Configurations test yolo
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
