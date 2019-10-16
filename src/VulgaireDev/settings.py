827fb8547316ee357863d5e0d5af7267828f5f1a
MATHONAT Romain <romain.mathonat@gmail.com>
2019-10-16 15:57:56 +0200

    Update settings.py

--- src/VulgaireDev/settings.py
+++ src/VulgaireDev/settings.py
@@ -30,7 +30,7 @@ VULGAIREDEV_DB_PASSWORD = os.environ.get("VULGAIREDEV_DB_PASSWORD", '')
 VULGAIREDEV_GMAIL_PASSWORD = os.environ.get("VULGAIREDEV_DB_PASSWORD", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.vulgairedev.fr',
                  '.vulgairedev.fr', '127.0.0.1', 'localhost', 'web']
